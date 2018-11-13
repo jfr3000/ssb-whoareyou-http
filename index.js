1a78b83ceb31131d0683f03d9d26804c0979796f
Julia Friesel <julia.friesel@gmail.com>
2018-11-13 17:12:04 +0100

    Add route for public key

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const url = require('url')
+
+exports.name = 'ssb-whoareyou-http'
+exports.version = require('./package.json').version
+exports.init = function (sbot) {
+  sbot.ws.use(function (req, res, next) {
+    if (req.method !== "GET") return next()
+    const u = url.parse(req.url)
+    if (u.pathname !== '/whoareyou') return next()
+
+    res.setHeader('Content-Type', 'text/plain')
+    sbot.whoami((err, key) => {
+      if (err) {
+        res.statusCode = 503
+        res.end(err.message)
+        return
+      }
+
+      res.end(key.id)
+    })
+  })
+}
