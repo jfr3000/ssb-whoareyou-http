dcb7a7a1d9edf41f3e89c151372495f06724a5d6
Julia Friesel <julia.friesel@gmail.com>
2018-12-13 17:34:54 +0100

    Revert "Return address instead of just id"
    This reverts commit 1bf3a115af6090009e0cb6e3c2e1c24819abb02d.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const url = require('url')
 const msScopes = require('multiserver-scopes')
-const msAddress = require('multiserver-address')
 
 exports.name = 'whoareyou-http'
 exports.manifest = {}
@@ -11,18 +10,20 @@ exports.init = function (sbot) {
 // set host in case it hasn't been set
   const ip = msScopes.host('public')
   sbot.config.host = ip
-
   sbot.ws.use(function (req, res, next) {
     if (req.method !== "GET") return next()
     const u = url.parse(req.url)
     if (u.pathname !== '/whoareyou') return next()
 
     res.setHeader('Content-Type', 'text/plain')
-    const address = sbot.getAddress()
-    // buggy, so we switch out the host
-    const parsed = msAddress.decode(address)
-    parsed[0][0].data[0] = ip
+    sbot.whoami((err, key) => {
+      if (err) {
+        res.statusCode = 503
+        res.end(err.message)
+        return
+      }
 
-    res.end(msAddress.encode(parsed))
+      res.end(key.id)
+    })
   })
 }
