c9d767ebd31b464b3d8d473057fab392fe7f0e71
Julia Friesel <julia.friesel@gmail.com>
2018-12-13 17:36:04 +0100

    Revert "Set host in sbot config"
    This reverts commit 48f40f5e59ba6b86cbb705e555a29697f55dd345.

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 'use strict'
 
 const url = require('url')
-const msScopes = require('multiserver-scopes')
 
 exports.name = 'whoareyou-http'
 exports.manifest = {}
 exports.version = require('./package.json').version
 exports.init = function (sbot) {
-// set host in case it hasn't been set
-  const ip = msScopes.host('public')
-  sbot.config.host = ip
   sbot.ws.use(function (req, res, next) {
     if (req.method !== "GET") return next()
     const u = url.parse(req.url)
